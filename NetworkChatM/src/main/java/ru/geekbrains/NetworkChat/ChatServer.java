24e3adf1cdf442324689941a55e4a2ad3d5f5b84
DRZTrigun <trigun1000@mail.ru>
2020-07-09 17:34:43 +0300

    Дописал открытие файла, ввод сообщения в файл, закрытие файла.

--- NetworkChatM/src/main/java/ru/geekbrains/NetworkChat/ChatServer.java
+++ NetworkChatM/src/main/java/ru/geekbrains/NetworkChat/ChatServer.java
@@ -5,7 +5,6 @@ public class ChatServer {
     public void start(int port){
         System.out.println("Server started on port: " + port);
     }
-
     public void stop() {
         System.out.println("Server stopped");
     }
